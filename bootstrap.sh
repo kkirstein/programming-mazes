5882a5b4979be7a8fb15c0a04d759d62c3839eb3
Kay-Uwe Kirstein <kayuwe.kirstein@uster.com>
2015-10-24 10:18:50 +0200

    + setup vagrant box

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# install and/or reload ntp daemon
+
+apt-get -y install ntp
+service ntp reload
+
+# setup correct timezone
+echo "Europe/Zurich" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+
