e764fe5a427eeca727cbd7078283826a196f46e4
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2015-12-10 22:16:45 +0100

    + setup dart project for webstorm

---
+++ dart/lib/mazes.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, Kay-Uwe Kirstein. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The dart library.
+///
+/// This is an awesome library. More dartdocs go here.
+library mazes;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/grid.dart';
---
+++ dart/lib/src/grid.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+// TODO: Put public facing types in this file.
+
+library mazes;
+
+/// Checks if you are awesome. Spoiler: you are.
+class Grid {
+  int rows;
+  int columns;
+
+  // constructor
+  Grid(this.rows, this.columns);
+}
