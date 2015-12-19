8fc5266cee2bbeda3d2b9ae0bca86f309fa1a95a
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2015-12-19 17:04:09 +0100

    + setup Cell & Grid class source files

--- dart/lib/mazes.dart
+++ dart/lib/mazes.dart
@@ -1,11 +1,9 @@
 // Copyright (c) 2015, Kay-Uwe Kirstein. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-/// The dart library.
+/// The mazes library.
 ///
-/// This is an awesome library. More dartdocs go here.
 library mazes;
 
-// TODO: Export any libraries intended for clients of this package.
-
-export 'src/grid.dart';
+part 'src/grid.dart';
+part 'src/cell.dart';
--- dart/lib/src/cell.dart
+++ dart/lib/src/cell.dart
@@ -1,15 +1,14 @@
 // Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-// TODO: Put public facing types in this file.
-
 part of mazes;
 
-/// Checks if you are awesome. Spoiler: you are.
-class Grid {
-  int rows;
-  int columns;
+/// Cell class a a single grid element
+class Cell {
+
+  int row, column;
+  Cell north, south, east, west;
 
   // constructor
-  Grid(this.rows, this.columns);
+  Cell(this.row, this.column);
 }
