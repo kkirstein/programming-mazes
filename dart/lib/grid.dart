f68bcc73a845aedc7d1767f147c3f230647849ee
Kay-Uwe Kirstein <kay-uwe@kirsteinhome.ch>
2016-01-09 22:40:54 +0100

    ! reformat code

--- dart/lib/grid.dart
+++ dart/lib/grid.dart
@@ -32,37 +32,43 @@ class Grid {
       return null;
     }
   }
+
   int _toIdx(int row, int column) {
     return row * columns + column;
   }
 
   // populate grid
   List<Cell> prepareGrid() {
-    return new List.generate(rows*columns, (i) => new Cell(i ~/ columns, i % columns));
+    return new List.generate(
+        rows * columns, (i) => new Cell(i ~/ columns, i % columns));
   }
 
   // configure cell neighborhood
   configureCells() {
-    _grid.forEach( (cell) {
+    _grid.forEach((cell) {
       var row = cell.row;
       var col = cell.column;
-      cell.north = getCell(row-1, col);
-      cell.south = getCell(row+1, col);
-      cell.west = getCell(row, col-1);
-      cell.east = getCell(row, col+1);
+      cell.north = getCell(row - 1, col);
+      cell.south = getCell(row + 1, col);
+      cell.west = getCell(row, col - 1);
+      cell.east = getCell(row, col + 1);
     });
   }
 
   // select random cell
-Cell randomCell() {
-  var row = _rand.nextInt(rows);
-  var col = _rand.nextInt(columns);
+  Cell randomCell() {
+    var row = _rand.nextInt(rows);
+    var col = _rand.nextInt(columns);
 
-  return getCell(row, col);
-}
+    return getCell(row, col);
+  }
+
+  // iterate over each row
 
   // dump grid for debug purpose
   dumpGrid() {
-    _grid.forEach((cell) { print(cell); });
+    _grid.forEach((cell) {
+      print(cell);
+    });
   }
 }
